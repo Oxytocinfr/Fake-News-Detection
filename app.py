083cf793dda99f468d811cd99088f06bbfdf6a35
Sujit Kumar Bhanja <shreban17@gmail.com>
2022-08-21 16:04:22 +0530

    update code

--- app.py
+++ app.py
@@ -27,6 +27,14 @@ def prediction():
     else:
         return render_template("prediction.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 
 if __name__ == '__main__':
     app.debug = True
